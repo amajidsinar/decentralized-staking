04d43dbf311a837d6fd5e2d090b7546461a3e15b
jedi <amajidsinar@gmail.com>
2021-09-26 21:58:32 +0700

    update dependencies

--- packages/react-app/.eslintrc.js
+++ packages/react-app/.eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   env: {
     browser: true,
   },
-  parser: "babel-eslint",
   // airbnb disabled after upgrade to cra 4 due to errors in our code
   extends: [/*"airbnb"*/ "plugin:prettier/recommended"],
   plugins: [
