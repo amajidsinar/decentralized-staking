ef904ca254442938173141c51a396a8d968b4d46
jedi <amajidsinar@gmail.com>
2021-09-26 22:00:09 +0700

    add time counter before deadline

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,30 +1,52 @@
 pragma solidity >=0.6.0 <0.7.0;
 
 import "hardhat/console.sol";
-import "./ExampleExternalContract.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "./ExampleExternalContract.sol";
 
+
+/**
+* @title Stacker Contract
+* @author scaffold-eth
+* @notice A contract that allow users to stack ETH
+*/
 contract Staker {
 
+  // External contract that will old stacked funds
   ExampleExternalContract public exampleExternalContract;
 
+  // Balances of the user's stacked funds
+  mapping(address => uint256) public balances;
+
+  // Staking threshold
+  uint256 public constant threshold = 1 ether;
+  uint256 public deadline = now + 30 seconds;
+
+  // Contract's Events
+  event Stake(address indexed sender, uint256 amount);
+
+  /**
+  * @notice Contract Constructor
+  * @param exampleExternalContractAddress Address of the external contract that will hold stacked funds
+  */
   constructor(address exampleExternalContractAddress) public {
     exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  /**
+  * @notice Stake method that update the user's balance
+  */
+  function stake() public payable {
+    // update the user's balance
+    balances[msg.sender] += msg.value;
+    
+    // emit the event to notify the blockchain that we have correctly Staked some fund for the user
+    emit Stake(msg.sender, msg.value);
+  }
 
-
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
-
-
-
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
-
-
-
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
-
+  function timeLeft() public view returns (uint256 timeleft) {
+    if( block.timestamp >= deadline ) {
+      return 0;
+    }
+    return deadline - block.timestamp; 
+  }
 }
